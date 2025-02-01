9880772e0ae142d1ab4516ed0456e3e1799951c8
lightman210567 <81330314+lightman210567@users.noreply.github.com>
2025-02-01 17:31:07 +0000

    fixed movement input listening
    - The property based input key selection was not working, so i switched back to using inputs defined in the input manager
    - Created code to select which input keys to use based on which player object the script is attatched to (1/2)
    
    Signed-off-by: lightman210567 <81330314+lightman210567@users.noreply.github.com>

--- Pong/Assets/Scripts/PaddleMovement.cs
+++ Pong/Assets/Scripts/PaddleMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 //using UnityEngine.InputSystem;
@@ -6,18 +7,37 @@ public class PaddleMovement : MonoBehaviour
 {
     // Initialise properties/variables
     public int player;
-    public InputAction upAction;
-    public InputAction downAction;
+    InputAction upAction;
+    InputAction downAction;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (player == 1)
+        {
+            Console.WriteLine("1");
+            upAction = InputSystem.actions.FindAction("Up1");
+            downAction = InputSystem.actions.FindAction("Down1");
+        }
+        else
+        {
+            Console.WriteLine("2");
+            upAction = InputSystem.actions.FindAction("Up2");
+            downAction = InputSystem.actions.FindAction("Down2");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Console.Write("Frame");
+        if (upAction.IsPressed())
+        {
+            throw new NotImplementedException();
+        }
+        if (downAction.IsPressed())
+        {
+            throw new NotImplementedException();
+        }
     }
 }

de95e23cdc54a8ca529b7a3f8c1e0077aca1551f
lightman210567 <81330314+lightman210567@users.noreply.github.com>
2025-02-01 21:24:34 +0000

    added a method to give the ball a random velocity
    - Created a method within the BallMovement script that gives the ball a random linear velocity on the x/y axes
    - This will be used at the start of the game to send the ball in a random direction
    
    Signed-off-by: lightman210567 <81330314+lightman210567@users.noreply.github.com>

--- Pong/Assets/Scripts/BallMovement.cs
+++ Pong/Assets/Scripts/BallMovement.cs
@@ -1,7 +1,26 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class BallMovement : MonoBehaviour
 {
+    // Initialise properties/variables
+    public float randomXMaximum;
+    public float randomYMaximum;
+    public float randomXMinimum;
+    public float randomYMinimum;
+    public Rigidbody2D rigidBody;
+
+    // A function to give the ball a random velocity
+    private void RandomVelocity()
+    {
+        float randomX = Random.Range(randomXMinimum, randomXMaximum);
+        float randomY = Random.Range(randomYMinimum, randomYMaximum);
+
+        rigidBody.linearVelocityX = randomX;
+        rigidBody.linearVelocityY = randomY;
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
